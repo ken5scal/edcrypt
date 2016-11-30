36d41e754f69a5b1ea2f9b6ee0fe44c881aa301a
ken5scal <kengoscal@gmail.com>
2016-12-01 01:23:09 +0900

    Initial AES

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"crypto/aes"
+	"fmt"
+)
+
+func main() {
+	cipherText := make([]byte, aes.BlockSize)
+	plainText := []byte("1234123412341234")
+	key := []byte("12341234123412341234123412341234") // AES-256(bit)
+	c, err:= aes.NewCipher(key); if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	c.Encrypt(cipherText, plainText)
+}
