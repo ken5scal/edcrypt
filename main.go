6a88c1880cbee48d45ba524fc8b1d4d0fe272f61
ken5scal <kengoscal@gmail.com>
2016-12-01 01:34:56 +0900

    FInish experiment

--- main.go
+++ main.go
@@ -6,14 +6,21 @@ import (
 )
 
 func main() {
-	cipherText := make([]byte, aes.BlockSize)
-	plainText := []byte("1234123412341234")
-	key := []byte("12341234123412341234123412341234") // AES-256(bit)
+
+	key := []byte("1234123412341234123412341234123") // Not AES-128, 256, 512(bit)
 	c, err:= aes.NewCipher(key); if err != nil {
 		fmt.Println(err.Error())
-		return
 	}
-	c.Encrypt(cipherText, plainText)
+
+	key = []byte("1234123412341234") // AES-128
+	c, err = aes.NewCipher(key); if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	cipherText := make([]byte, aes.BlockSize)
+	plainText := []byte("1234123412341234")
+
+	c.Encrypt(cipherText, plainText) // Input/Output must be 16bits large
 	fmt.Println(cipherText)
 
 	c.Decrypt(plainText, cipherText)
